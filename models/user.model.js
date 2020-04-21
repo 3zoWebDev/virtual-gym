6585d00674e46d0a30373c2cad0e37f31931bfc6
Abdulaziz-Alsubhi-GA <3zoalsubhi@gmail.com>
2020-04-22 02:47:27 +0300

    fix

--- models/user.model.js
+++ models/user.model.js
@@ -7,14 +7,11 @@ const userSchema = new Schema(
     name:  String,
     email: String,
     password: String,
-<<<<<<< HEAD
 
-=======
     isAdmin:{
       type:Boolean,
-      default:false 
+      default:false
     }
->>>>>>> 54e0128bd466f5a8ccefc188eaa094790d3dc64e
   },
   { timestamps: true }
 );
